a9d6157241e4ba55d0723b4ca6a723550cb3c85d
PHATTT <trantanphat125678@gmail.com>
2023-03-09 09:45:14 +0700

    TranTanPhat_1911062072 => by (phat)

--- WebApplication1/WebApplication1/WeatherForecast.cs
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -1,11 +1,12 @@
-namespace WebApplication1
+Snamespace WebApplication1
 {
     public class WeatherForecast
     {
-        //public DateTime Date { get; set; }
+        public DateTime Date { get; set; }
+
         public int Id { get; set; }
 
-        public int TemperatureC { get; set; }
+        public String TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
