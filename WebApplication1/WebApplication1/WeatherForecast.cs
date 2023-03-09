383230c3ddec70a8a56d206b3817d403eb21b5eb
PHATTT <trantanphat125678@gmail.com>
2023-03-09 09:22:03 +0700

    TranTanPhat_1911062072

--- WebApplication1/WebApplication1/WeatherForecast.cs
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace WebApplication1
     public class WeatherForecast
     {
         //public DateTime Date { get; set; }
-        public int Id { get; set; }
+        public String Id { get; set; }
 
         public int TemperatureC { get; set; }
 
